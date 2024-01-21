8567b8ab8a7e4420b19822c8c47ade46b65becd3
Analexa <analexa100@gmail.com>
2024-01-21 18:01:25 +0100

    generate route + provider + basic controller + empty service + unitTest

---
+++ app/Http/Controllers/apiUrlController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class apiUrlController extends Controller
+{
+
+    public function index()
+    {
+       dump('muestra');
+       die();
+
+
+       // return view('admin.users.index', compact('users'));
+    }
+
+
+
+}
---
+++ app/Services/UrlTransformer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services;
+
+class UrlTransformer
+{
+    public function transformUrl(string $url, bool $isRequired)
+    {
+
+        return $url;
+    }
+}
