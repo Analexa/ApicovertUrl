e5e9fc63d9d391b24b9342d9d793a35d0cadfade
Analexa <analexa100@gmail.com>
2024-01-21 21:04:39 +0100

    apiurl environment variable + call service

--- app/Services/UrlTransformer.php
+++ app/Services/UrlTransformer.php
@@ -2,11 +2,15 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Config;
+
 class UrlTransformer
 {
     public function transformUrl(string $url, bool $isRequired)
     {
+        $apiUrl = env('TINYURL_API_URL') . '?url=' . urlencode($url);
+        $response = file_get_contents($apiUrl);
 
-        return $url;
+        return $response;
     }
 }
